5aa6c523ae259bc6b6c99cc9bff061123ced5fce
KansaiUser <hitorunajp@gmail.com>
2023-10-29 23:46:07 +0900

    added tangent

--- tangentAnimation.py
+++ tangentAnimation.py
@@ -18,7 +18,6 @@ X = radius * np.cos(t) + amplitude * np.sin(frequency * t)
 Y = radius * np.sin(t) + amplitude * np.cos(frequency * t)
 
 
-
 def calculate_unit_tangent_vector(frame_number):
     if frame_number < 0 or frame_number >= num_points:
         return None  # Frame number out of range
@@ -34,13 +33,23 @@ def calculate_unit_tangent_vector(frame_number):
     tangent_vector = (dx / magnitude, dy / magnitude)
     return tangent_vector
 
+start_frame = 0
+tangent_vector = calculate_unit_tangent_vector(start_frame)
+x_tangent, y_tangent = X[start_frame], Y[start_frame]
+dx, dy = tangent_vector
+
 fig, ax = plt.subplots()
 ax.set_xlim(-1.5, 1.5)
 ax.set_ylim(-2.0, 2.0)
-ax.plot(X, Y, marker='o', markersize=3, linestyle='-')
+
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_title('Circular Trajectory with Sinusoidal Oscillation of X and Y')
 ax.grid(True)
 ax.axis('equal')
+
+ax.plot(X, Y, marker='o', markersize=3, linestyle='-')
+tangent = plt.quiver(x_tangent, y_tangent, dx, dy, color='red', angles='xy',
+          scale_units='xy', scale=1, width=0.005)
+
 plt.show()
