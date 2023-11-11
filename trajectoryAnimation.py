125eef49e6778d353140c6763a889465583284bc
KansaiUser <hitorunajp@gmail.com>
2023-11-12 01:25:17 +0900

    first animation

--- trajectoryAnimation.py
+++ trajectoryAnimation.py
@@ -3,6 +3,7 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from matplotlib.animation import FuncAnimation
 
 # 1. Code to get data for trajectory
 
@@ -21,10 +22,10 @@ frequency = 5.0
 X = radius * (np.cos(t) + amplitude * np.sin(frequency * t))
 Y = radius * (np.sin(t) + amplitude * np.cos(frequency * t))
 
-XL = (radius+1) * (np.cos(t) + amplitude * np.sin(frequency * t))
-YL = (radius+1) * (np.sin(t) + amplitude * np.cos(frequency * t))
-XR = (radius-1) * (np.cos(t) + amplitude * np.sin(frequency * t))
-YR = (radius-1) * (np.sin(t) + amplitude * np.cos(frequency * t))
+XL = (radius + 1) * (np.cos(t) + amplitude * np.sin(frequency * t))
+YL = (radius + 1) * (np.sin(t) + amplitude * np.cos(frequency * t))
+XR = (radius - 1) * (np.cos(t) + amplitude * np.sin(frequency * t))
+YR = (radius - 1) * (np.sin(t) + amplitude * np.cos(frequency * t))
 
 data = pd.DataFrame({'fr': frame_numbers, 'X': X, 'Y': Y, 'XL': XL, 'YL': YL,
                     'XR': XR, 'YR': YR})
@@ -53,9 +54,14 @@ def calculate_unit_tangent_vector(frame_number):
 def isin(X, Y):
     X = X - XC
     Y = Y - YC
-    X_ = X*cos_theta + Y*sin_theta
-    Y_ = -X*sin_theta + Y*cos_theta
-    return (-width/2 <= X_) & (X_ <= width/2) & (-height/2 <= Y_) & (Y_ <= height/2)
+    X_ = X * cos_theta + Y * sin_theta
+    Y_ = -X * sin_theta + Y * cos_theta
+    return (
+        (-width / 2 <= X_)
+        & (X_ <= width / 2)
+        & (-height / 2 <= Y_)
+        & (Y_ <= height / 2)
+    )
 
 
 # Rotation function
@@ -86,23 +92,27 @@ height = 4
 # function to calculate the instantaneous rectangle
 def calculate_rectangle(XD, YD, XC, YC):
     theta = np.arctan2(YD, XD)
-    print("Angle:",math.degrees(theta))
+    print("Angle:", math.degrees(theta))
     cos_theta = np.cos(theta)
     sin_theta = np.sin(theta)
 
-    rectangle_x = [XC + 0.5 * width * cos_theta - 0.5 * height * sin_theta,
-                   XC - 0.5 * width * cos_theta - 0.5 * height * sin_theta,
-                   XC - 0.5 * width * cos_theta + 0.5 * height * sin_theta,
-                   XC + 0.5 * width * cos_theta + 0.5 * height * sin_theta,
-                   XC + 0.5 * width * cos_theta - 0.5 * height * sin_theta]
-    
-    rectangle_y = [YC + 0.5 * width * sin_theta + 0.5 * height * cos_theta,
-                   YC - 0.5 * width * sin_theta + 0.5 * height * cos_theta,
-                   YC - 0.5 * width * sin_theta - 0.5 * height * cos_theta,
-                   YC + 0.5 * width * sin_theta - 0.5 * height * cos_theta,
-                   YC + 0.5 * width * sin_theta + 0.5 * height * cos_theta]
+    rectangle_x = [
+        XC + 0.5 * width * cos_theta - 0.5 * height * sin_theta,
+        XC - 0.5 * width * cos_theta - 0.5 * height * sin_theta,
+        XC - 0.5 * width * cos_theta + 0.5 * height * sin_theta,
+        XC + 0.5 * width * cos_theta + 0.5 * height * sin_theta,
+        XC + 0.5 * width * cos_theta - 0.5 * height * sin_theta,
+    ]
 
-    return rectangle_x,rectangle_y
+    rectangle_y = [
+        YC + 0.5 * width * sin_theta + 0.5 * height * cos_theta,
+        YC - 0.5 * width * sin_theta + 0.5 * height * cos_theta,
+        YC - 0.5 * width * sin_theta - 0.5 * height * cos_theta,
+        YC + 0.5 * width * sin_theta - 0.5 * height * cos_theta,
+        YC + 0.5 * width * sin_theta + 0.5 * height * cos_theta,
+    ]
+
+    return rectangle_x, rectangle_y
 
 
 # 3. Start feature
@@ -118,10 +128,10 @@ rectangle_x, rectangle_y = calculate_rectangle(XD, YD, XC, YC)
 
 
 fig, ax = plt.subplots()
-ax.plot(data['X'], data['Y'],color='b')
-ax.plot(data['XL'], data['YL'],color='r')
-ax.plot(data['XR'],data['YR'],color='g')
-ax.axis('equal')
+ax.plot(data["X"], data["Y"], color="b")
+ax.plot(data["XL"], data["YL"], color="r")
+ax.plot(data["XR"], data["YR"], color="g")
+ax.axis("equal")
 ax.grid(True)
 
 # 5. Plot the dynamic features once
@@ -129,15 +139,50 @@ ax.grid(True)
 if tangent_vector is not None:
     x_tangent, y_tangent = X[example_frame], Y[example_frame]
     dx, dy = tangent_vector
-    tangent = ax.quiver(x_tangent, y_tangent, dx, dy, color='violet', angles='xy', scale_units='xy', scale=1, width=0.005)
+    tangent = ax.quiver(
+        x_tangent,
+        y_tangent,
+        dx,
+        dy,
+        color="violet",
+        angles="xy",
+        scale_units="xy",
+        scale=1,
+        width=0.005,
+    )
+
+car = ax.scatter(XC, YC, color="r")
+(rec1,) = ax.plot(rectangle_x, rectangle_y, color="purple")
+# rec2 = ax.fill(rectangle_x, rectangle_y, color='purple', alpha=0.4)
 
 
-rec1 = ax.plot(rectangle_x, rectangle_y, color='purple')
-rec2 = ax.fill(rectangle_x, rectangle_y, color='purple', alpha=0.4)
+# 5. Modifies rec1,rec2 and tangent_vector
+# receives frame
+def animate(frame):
+    tangent_vector = calculate_unit_tangent_vector(frame)
+    # x_tangent, y_tangent = X[frame], Y[frame]
+    # dx, dy = tangent_vector
+    XC, YC = X[frame], Y[frame]
+    XD, YD = tangent_vector
+    rectangle_x, rectangle_y = calculate_rectangle(XD, YD, XC, YC)
+
+    car.set_offsets([XC, YC])
+
+    tangent.set_UVC(XD, YD)
+    tangent.set_offsets([XC, YC])
+    rec1.set_data(rectangle_x, rectangle_y)
+    # rec2.set_data(rectangle_x, rectangle_y)
+    # rec2.get_paths()[0].vertices[:, 0] = rectangle_x
+    # rec2.get_paths()[0].vertices[:, 1] = rectangle_y
+
+    return (
+        tangent,
+        car,
+        rec1,
+    )  # rec2,
+
+
+animation = FuncAnimation(fig, animate, frames=np.arange(0, num_points, 1), blit=True)
 
 
 plt.show()
-
-
-
-
